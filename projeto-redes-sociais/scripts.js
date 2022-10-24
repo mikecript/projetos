d9ee03e44303fe9ac0e7caaf40eeb33fa3110088
Maicol Santos <maicolestudosacademicos@gmail.com>
2022-10-24 15:07:24 -0300

    version 1.3
    atualização de projetos

---
+++ projeto-redes-sociais/scripts.js
@@ -0,0 +1,9 @@
+/*
+let icones = document.getElementsByClassName('.icones')
+
+function click() {
+let animacao = icones.item(onclick(document.body.style.border('50px 50px 50px black')))
+return animacao
+ }
+
+ */
