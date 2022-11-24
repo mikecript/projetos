ed802e5812f07c9c64eb2a99645646cd38d8edb7
Maicol Santos <maicolestudosacademicos@gmail.com>
2022-11-24 11:23:01 -0300

    update

---
+++ desafioResponsivo/scripts.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu');
+
+ function abrirMenu() {
+
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    } else{
+        menu.style.display = 'block'
+    }
+   
+ }
+
+
+ function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'block'
+    } else{
+        menu.style.display = 'none'
+    }
+ }
